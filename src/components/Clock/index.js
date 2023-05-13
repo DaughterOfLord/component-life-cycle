9433efd5fa22dcc47a48432d2ae12157d8c3d91e
deepika <deepikadasari78@gmail.com>
2023-05-13 17:58:58 +0000

    components life cycle

---
+++ src/components/Clock/index.js
@@ -0,0 +1,26 @@
+import {Component} from 'react'
+import './index.css'
+
+class Clock extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {date: new Date()}
+    console.log('constructor called')
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount called')
+  }
+
+  render() {
+    const {date} = this.state
+    console.log('render called')
+    return (
+      <div className="clock-container">
+        <h1 className="heading">Clock</h1>
+        <p className="time">{date.toLocaleTimeString()}</p>
+      </div>
+    )
+  }
+}
+export default Clock
